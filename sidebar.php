1c453a87da5c8a338addbf23c1d69ed875207cb9
Deepashree Deshmukh <18piscean@gmail.com>
2019-04-03 18:56:26 +0530

    adjust styles

--- sidebar.php
+++ sidebar.php
@@ -15,7 +15,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <aside id="secondary" class="widget-area">
     <div class="author-info">
         <h1>author's name</h1>
-        <img src="" alt="author">
+        <img src="D18JBSNWwAA5uGk.png" alt="author">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc non blandit massa enim nec. Amet porttitor eget dolor morbi non arcu. Cursus turpis massa tincidunt dui. A iaculis at erat pellentesque adipiscing.</p>
     </div>
     <?php dynamic_sidebar( 'sidebar-1' ); ?>
